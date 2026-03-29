e61725033fb5548eb0969bc093ec63b38e72f7a5
Lokesh <lokesh0patra@gmail.com>
2026-03-30 03:40:28 +0530

    tagged as Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,12 +46,42 @@ export const metadata: Metadata = {
     siteName: "Lokesh Patra's Portfolio",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "/site-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Lokesh Patra's Portfolio Logo",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Lokesh Patra - Sr. Associate DSML Developer ",
     description: "Engineer by profession, Thinker at heart.",
+    images: ["/site-logo.png"],
   },
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/android-chrome-192x192.png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/android-chrome-512x512.png",
+      },
+    ],
+  },
+  manifest: "/favicon_io/site.webmanifest",
   verification: {
     google: "e4_H-qo2KANrVThYHbdxqScXu_bONS_logrBVk8MYuE",
   },
